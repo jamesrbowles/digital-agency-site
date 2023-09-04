587f765a078e3740092e5e9a5c58d32a7663629f
James Bowles <jamesrbowles@gmail.com>
2023-09-04 15:07:20 +0200

    start of slug project page

--- app/project/[slug]/page.tsx
+++ app/project/[slug]/page.tsx
@@ -1,3 +1,15 @@
-export default function Project() {
-  return <div>page</div>;
+import projects from '@/data/projects';
+
+type paramsSlug = {
+  slug: string;
+};
+
+export default function Project({ params }: { params: paramsSlug }) {
+  const slug = params.slug;
+
+  const project = projects.find(
+    (project) => slug === project.title.replace(/ /g, '-').toLowerCase()
+  );
+
+  return <div>{project?.title}</div>;
 }
